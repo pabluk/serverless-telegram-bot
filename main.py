c3b88498866bb2db03c79cedca3d7e052667ffa9
Pablo SEMINARIO <pablo@seminar.io>
2018-09-05 01:27:29 +0200

    Move `bot` instance into global scope
    In order to apply https://cloud.google.com/functions/docs/bestpractices/tips

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import os
 import telegram
 
+bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
+
 
 def webhook(request):
-    bot = telegram.Bot(token=os.environ["TELEGRAM_TOKEN"])
     if request.method == "POST":
         update = telegram.Update.de_json(request.get_json(force=True), bot)
         chat_id = update.message.chat.id
